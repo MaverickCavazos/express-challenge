da4ad94122891f7e2470def1f89a68ff84c867ea
Maverick <Mav33987@gmail.com>
2022-01-30 18:54:25 -0600

    refactored apiroutes

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
 const newNote = require('../db/notes')
 
-router.get('/api/notes', (req, res) => {
-   newNote.getNotes().then((notes) => {
-       return res.json(notes);
-   }) .catch((err) => res.status(404).json(err))
-   
+router.get('/notes', (req, res) => {
+    newNote.getNotes().then((notes) => {
+        console.log('here')
+        console.log(notes)
+        return res.json(notes);
+    }).catch((err) => res.status(404).json(err))
+
 });
+
+
 
 /* router.delete ('/api/notes/:id', (req, res) => {
     */
 /* }); */
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     newNote.addNotes(req.body).then((notes) => {
         return res.json(notes);
-    }) .catch((err) => res.status(404).json(err))
+    }).catch((err) => res.status(404).json(err))
 });
 
 module.exports = router;
